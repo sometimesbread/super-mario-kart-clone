c94862407e3890ec1d129ed18624f553fb86360b
sometimesbread <82068136+sometimesbread@users.noreply.github.com>
2022-01-12 14:46:36 -0800

    been awhile since the last commit

--- raylib/main.cpp
+++ raylib/main.cpp
@@ -9,22 +9,20 @@
 #include "bg_hills.h"
 #include "mario_spritesheet.h"
 #include "mariocircuit-1collision.h"
+#include "lap.h"
+#include "particles.h"
 
 //#include make header file that only stores a 2d array of the image (using the color compression)
 using namespace std;
 
 int main(void)
 {
-    const float mode7tomapsize = 1.024;
-
     //mode 7 transformations
     int xPrime = 0;
     int yPrime = 0;
 
     float a = 1.0f;
     float b = 0.0f;
-    float c = 0.0f;
-    float d = 1.0f;
 
     float h = 315.5f; //yeah its weird but its perfectly centered on the starting line
     float v = 808.0f;
@@ -32,31 +30,35 @@ int main(void)
     float xn = 128.0f + h;
     float yn = 112.0f + v;
 
-    float aChange = 8000;
+    const float aChange = 8000;
 
     //time  
     float previousTime = 0.0f;
     float deltaTime = 0.0f;
 
+    unsigned long long elapsedFrames = 0;
+
     //kart physics
 
-    float angle = 4.71239f;
+    float angle = 4.71238897f;
     float forwardX = 0.0f;
     float forwardY = 0.0f;
 
     float velocity = 0.0f;
-    float acceleration = 0.25f;
-    float maxVelocity = 1.25f;
-    float drag = 0.125f;
+    const float acceleration = 0.25f;
+    const float maxVelocity = 1.25f;
+    const float drag = 0.125f;
 
-    float offroadVelocityMultiplier = 0.5f;
+    const float offroadVelocityMultiplier = 0.75f;
 
     float torque = 0.0f;
-    float torqueAcceleration = 0.06f;
-    float maxTorque = 0.08f;
-    float torqueDrag = 0.015f;
+    const float torqueAcceleration = 0.06f;
+    const float maxTorque = 0.08f;
+    const float torqueDrag = 0.015f;
 
-    float drifting = false;
+    bool drifting = false;
+
+    const float kartCollisionOffset = -0.5f;
 
     //kart drawing
 
@@ -86,7 +88,17 @@ int main(void)
     int kartAnimationFrame = 0;
 
     int spriteReversed = 0; //0 = not reversed, 1 = reversed
-    float timeAboutToDrift = 0.0;
+    float timeAboutToDrift = 0.0f;
+    float accumulatedRotation = 0.0f;
+
+    unsigned int originalWheelColor = 0x5b7a7aff;
+    unsigned int rotatedWheelColor = 0x385b5b;
+
+    int wiggleRate = 1;
+    int wiggledKartPos = kartPosY + wiggleRate;
+
+    bool drawDirtParticles = false;
+    bool drawDriftParticles = false;
 
     //map
     float mapScaling = 0.5;
@@ -130,7 +142,7 @@ int main(void)
         if(torque > maxTorque) { torque = maxTorque; }
         if(torque < -maxTorque) { torque = -maxTorque; }
 
-        //get the forward vector and sideways vector
+        //calculate these vectors. thank you random man on stackoverflow for this code
         forwardX = sin(angle);
         forwardY = cos(angle);
 
@@ -138,7 +150,13 @@ int main(void)
         //(0,0,0) = track, (255,255,255) = offroad, (127,127,127) = endline, (90,90,90) = coin, (100,100,100) = wall/bounceback, (200,200,200) = block
         
         //check if on track
-        if(mariocircuit1CollisionColorLookup[mariocircuit1ColorList[(int)floor(h * mode7tomapsize)][(int)floor(v * mode7tomapsize)]] == 0xffffffff) { velocity *= offroadVelocityMultiplier; cout << "Offroad" << endl; }
+        //holy shit i spent a whole week on this one line aaaaaaaaaaaaaaaaaaaaaa
+        //thank you to my brother for finding the bug in this one line in 30 seconds. i spent like a week trying to fix the bug and he found it instantly lmao.
+        if(mariocircuit1CollisionColorLookup[mariocircuit1CollisionColorList[128 - (int)floor(xn / 8 - forwardX * kartCollisionOffset)][128 - (int)floor(yn / 8 - forwardY * kartCollisionOffset)]] == 0xffffffff) { velocity *= offroadVelocityMultiplier; drawDirtParticles = true; }
+        else { drawDirtParticles = false; }
+
+        //check if the player hits a wall
+        if(mariocircuit1CollisionColorLookup[mariocircuit1CollisionColorList[128 - (int)floor(xn / 8 - forwardX * kartCollisionOffset)][128 - (int)floor(yn / 8 - forwardY * kartCollisionOffset)]] == 0x646464ff) { velocity = -velocity * 4; }
 
         //update mode 7 variables
         v -= forwardY * velocity;
@@ -160,31 +178,44 @@ int main(void)
 
             //draw track
             for (int y = 0; y < 224 - horizonHeight; y++)
-            {
+            { 
+                //calculate depth
                 float scl = aChange / 224 / (y+1);
+                //float scl = 1;
                 a = scl * forwardY * mapScaling; //using forwardY because it is already calculated and calculating cos and sin again is a waste
                 b = scl * forwardX * mapScaling;
 
+                //calculate the "bases" of xprime and yprime so i dont murder the cpu
                 float xPrimebase = a * (h - xn) + b * (y +  v - yn) + xn;
-                float yPrimebase = -b * (h - xn) + a * (y+ v - yn) + yn;
+                float yPrimebase = -b * (h - xn) + a * (y + v - yn) + yn;
 
                 for (int x = 0; x < 256; x++)
                 {
                     xPrime = floor(xPrimebase + a * x);
                     yPrime = floor(yPrimebase + -b * x);
+
+                    //check if the current pixel is in the map, and draw the map texture if it is. if it not, draw the repeating texture
                     if(xPrime < 1024 && xPrime > 0 && yPrime > 0 && yPrime < 1024)
                     {
                         DrawPixel(x, y + horizonHeight, GetColor(mariocircuit1ColorLookup[mariocircuit1ColorList[xPrime][yPrime]]));
-                        //DrawPixel(x, y + horizonHeight, GetColor(mariocircuit1CollisionColorLookup[mariocircuit1CollisionColorList[(int)floor(xPrime / 8)][(int)floor(yPrime / 8)]]));
+
+                        //show collision map instead of color map (debug)
+                        //DrawPixel(x, y + horizonHeight, GetColor(mariocircuit1CollisionColorLookup[mariocircuit1CollisionColorList[128 - (int)floor(xPrime / 8)][128 - (int)floor(yPrime / 8)]]));
                     }
                     else
                     {
-                        DrawPixel(x, y + horizonHeight, GetColor(outOfBoundsColorLookup[outOfBoundsColorList[abs(xPrime % 8)][abs(yPrime % 8)]]));
+                        DrawPixel(x, y + horizonHeight, GetColor(outOfBoundsColorLookup[outOfBoundsColorList[abs(xPrime) % 8][abs(yPrime) % 8]]));
                     }
                 }
             }
 
         //draw kart
+
+        accumulatedRotation += velocity;
+        if(fmod(accumulatedRotation, 7.5) > 3.75) { marioSpritesheetColorLookup[10] = rotatedWheelColor; kartPosY = wiggledKartPos; }
+        else { marioSpritesheetColorLookup[10] = originalWheelColor; kartPosY = wiggledKartPos - wiggleRate; }
+
+        //calculate the current sprite based off of the current torque
         if(torque < 0.01) { kartAnimationFrame = 0; spriteReversed = 0; }
         if(torque > -0.01) { kartAnimationFrame = 0; spriteReversed = 0; }
 
@@ -194,6 +225,8 @@ int main(void)
         if(torque > 0.05) { kartAnimationFrame = 2; spriteReversed = 1; timeAboutToDrift += deltaTime; }
         if(torque < -0.05) { kartAnimationFrame = 2; spriteReversed = 0; timeAboutToDrift += deltaTime; }
 
+        if(!IsKeyDown(KEY_A) || !IsKeyDown(KEY_D)) { timeAboutToDrift = 0; }
+
         for (int x = 0; x < kartSize; x++)
         {
             for (int y = 0; y < kartSize; y++)
@@ -201,110 +234,35 @@ int main(void)
                 DrawPixel((kartPosX - halfKartSize) + x, (kartPosY - halfKartSize) + y + (112 + halfKartSize), GetColor(marioSpritesheetColorLookup[marioSpritesheetColorList[kartState * kartSize + y][kartAnimationFrame * kartSize + x + (spriteReversed * kartSize * 11)]]));
             }
         }
-            
+
+        //draw big dirt particles (sorry for the hardcoded numbers im going insane from coding and i dont feel like making variables)
+        if(drawDirtParticles)
+        {
+            if(velocity > 0.1f || velocity < -0.1f)
+                for (int x = 0; x < 32; x++)
+                {
+                    for (int y = 0; y < 32; y++)
+                    {
+                        //left particles
+                        DrawPixel(kartPosX + 11 + x + 4 * floor(sinf(accumulatedRotation * 2)), kartPosY + 150 + y + 2 * floor(1 - sinf(accumulatedRotation * 2)), GetColor(particlesColorLookup[particles[y][x]]));
+                        //right particles
+                        DrawPixel(256 - (kartPosX + 11 + x + 4 * floor(sinf(accumulatedRotation * 2))), kartPosY + 150 + y + 2 * floor(1 - sinf(accumulatedRotation * 2)), GetColor(particlesColorLookup[particles[y][x]]));
+                    }
+                }
+               
+        }
+        
+        cout << velocity << endl;
 
         EndDrawing();
-
+        
+        //update the time variables
         deltaTime = (chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count() / 1000000000 - previousTime);
         previousTime = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count() / 1000000000;
+        elapsedFrames++;
     }
-
+    
     //deinitialization
     CloseWindow();
     return 0;
 }
-
-/*
-    #draw horizon
-
-    for y in range(horizonHeight):
-        for x in range(256):
-            drawToScreen(x, y, int(x + angle * hillTurnSpeed) % hillTextureWidth, y, hillTextureColorList)
-            if(treeTexture.get_at((int(x + angle * treeTurnSpeed) % hillTextureWidth, y)) != ((0,0,0))):
-                drawToScreen(x, y, int(x + angle * treeTurnSpeed) % hillTextureWidth, y, treeTextureColorList)
-            
-    #draw track
-
-    #probably do something like this https://www.geeksforgeeks.org/multiprocessing-python-set-1/ because this code sucks
-
-    for y in range(224 - horizonHeight):
-        scl = aChange / 224 / (y+1)
-        a = scl * forwardY * mapScaling #using forwardY because it is already calculated and calculating cos and sin again is a waste
-        b = scl * forwardX * mapScaling
-
-        xPrimebase = a * (h - xn) + b * (y +  v - yn) + xn
-        yPrimebase = -b * (h - xn) + a * (y+ v - yn) + yn
-        
-        #inputs = [a, b]
-
-        #processed_list = Parallel(n_jobs=numCores)(delayed(modeSevenTransformation)(i, inputs) for i in inputs)
-
-        for x in range(256):
-            xPrime = floor(xPrimebase + a * x)
-            yPrime = floor(yPrimebase + -b * x)
-            if(xPrime < 1024 and xPrime > 0 and yPrime > 0 and yPrime < 1024):
-                drawToScreen(x, y + horizonHeight, xPrime, yPrime, backgroundTextureColorList)
-            else:
-                drawToScreen(x, y + horizonHeight, xPrime % 8, yPrime % 8, repeatingTextureColorList)
-
-    #draw kart
-
-    if(torque < 0.01):
-        kartAnimationFrame = 0
-        spriteReversed = 0
-    
-    if(torque > -0.01):
-        kartAnimationFrame = 0
-        spriteReversed = 0
-
-    if(torque > 0.01):
-        kartAnimationFrame = 1
-        spriteReversed = 1
-
-    if(torque < -0.01):
-        kartAnimationFrame = 1
-        spriteReversed = 0
-
-    if(torque > 0.05):
-        kartAnimationFrame = 2
-        spriteReversed = 1
-        timeAboutToDrift += deltaTime
-    
-    if(torque < -0.05):
-        kartAnimationFrame = 2
-        spriteReversed = 0
-        timeAboutToDrift += deltaTime
-
-    #if(torque > 0.08):
-    #    kartAnimationFrame = 3
-    #    spriteReversed = 1
-    
-    #if(torque < -0.08):
-    #    kartAnimationFrame = 3
-    #    spriteReversed = 0
-
-    #if(torque > 0.15):
-    #    kartAnimationFrame = 4
-    #    spriteReversed = 1
-    
-    #if(torque < -0.15):
-    #    kartAnimationFrame = 4
-    #    spriteReversed = 0
-
-    kartPosX = 128 - kartSize
-    kartPosY = 128 - kartSize
-
-
-    for x in range(kartSize):
-        for y in range(kartSize):
-            #if marioSpritesSurface.get_at(((kartAnimationFrame * kartSize + x + spriteReversed * (kartSize * 11)), kartState.value * kartSize + y)) !=((0,0,0)):
-            if marioTextureColorList[kartState.value * kartSize + y][kartAnimationFrame * kartSize + x + spriteReversed * (kartSize * 11)] != (0,0,0):
-                drawToScreen((kartPosX + x + floor(kartSize / 2)), kartPosY + y + floor(kartSize / 2), (kartAnimationFrame * kartSize + x + spriteReversed * (kartSize * 11)), kartState.value * kartSize + y, marioTextureColorList)
-
-    display.update()
-    deltaTime = (time.time_ns() / 1000000000 - previousTime)
-    print(1/deltaTime)
-    previousTime = time.time_ns() / 1000000000
-
-pygame.quit()
-*/
