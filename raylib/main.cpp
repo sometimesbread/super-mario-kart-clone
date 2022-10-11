07a8d2d33e1f93adc2ccec78eec125040feb9dc3
sometimesbread <82068136+sometimesbread@users.noreply.github.com>
2022-10-11 16:24:56 -0700

    replaced if statements with switch statement

--- raylib/main.cpp
+++ raylib/main.cpp
@@ -98,8 +98,8 @@ int main(void)
 
     //lap count
     int currentCheckpoint = 5;
-    int currentLap = 4;
-    int displayedLap = 4;
+    int currentLap = 1;
+    int displayedLap = 1;
 
     //map
     float mapScaling = 0.5;
@@ -168,12 +168,32 @@ int main(void)
         if(currentCollisionColor == 0x646464ff) { velocity = -velocity * 4; }
 
         //update current checkpoint
-        if(currentCheckpointColor == 0xa0a0aff) { if(currentCheckpoint == 5) { currentLap++; currentCheckpoint = 0; } }
-        if(currentCheckpointColor == 0x141414ff) { if(currentCheckpoint == 0) { currentCheckpoint = 1; } }
-        if(currentCheckpointColor == 0x1e1e1eff) { if(currentCheckpoint == 1) { currentCheckpoint = 2; } }
-        if(currentCheckpointColor == 0x282828ff) { if(currentCheckpoint == 2) { currentCheckpoint = 3; } }
-        if(currentCheckpointColor == 0x323232ff) { if(currentCheckpoint == 3) { currentCheckpoint = 4; } }
-        if(currentCheckpointColor == 0x3c3c3cff) { if(currentCheckpoint == 0) { currentLap--; currentCheckpoint = 5; } { if(currentCheckpoint == 4) { currentCheckpoint = 5; } } }
+        switch(currentCheckpointColor)
+        {
+            case 0xa0a0aff:
+            if(currentCheckpoint == 5 || currentCheckpoint == 1) { currentLap++; currentCheckpoint = 0; }
+            break;
+
+            case 0x141414ff:
+            if(currentCheckpoint == 0 || currentCheckpoint == 2) { currentCheckpoint = 1; }
+            break;
+
+            case 0x1e1e1eff:
+            if(currentCheckpoint == 1 || currentCheckpoint == 3) { currentCheckpoint = 2; }
+            break;
+
+            case 0x282828ff:
+            if(currentCheckpoint == 2 || currentCheckpoint == 4) { currentCheckpoint = 3; }
+            break;
+
+            case 0x323232ff:
+            if(currentCheckpoint == 3 || currentCheckpoint == 5) { currentCheckpoint = 4; }
+            break;
+
+            case 0x3c3c3cff:
+            if(currentCheckpoint == 0) { currentLap--; currentCheckpoint = 5; } if(currentCheckpoint == 4) { currentCheckpoint = 5; }
+            break;
+        }
         
         cout << currentCheckpoint << " " << currentLap << endl;
 
